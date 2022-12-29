029a18b224bdb5ffc413a233a62a127a7b1c3afe
不吃萝卜 <57123150+thn-0@users.noreply.github.com>
2022-12-29 15:20:46 +0800

    Update conf.py

--- MySQL备份脚本/conf.py
+++ MySQL备份脚本/conf.py
@@ -2,7 +2,7 @@
 # _*_ coding:utf-8 _*_
 # author:不吃萝卜
 # 备份配置文件，单库备份脚本
-conn_dict = dict(host='43.139.155.121', user='root', passwd='Hzmc321#', db='-A', port='33306')
+conn_dict = dict(host='127.0.0.1', user='root', passwd='mysql', db='-A', port='3306')
 # contact_user = dict(address='smtp.163.com',sender='xxxxxx@163.com',password='xxxxxx')
 # note_user = ('xxxxxx@126.com',)
 backup_path = "/backup/"
