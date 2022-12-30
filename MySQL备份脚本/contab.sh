958aad296c749d9dc44b1855530b16aa317554a7
不吃萝卜 <57123150+thn-0@users.noreply.github.com>
2022-12-30 10:26:47 +0800

    Update contab.sh

--- MySQL备份脚本/contab.sh
+++ MySQL备份脚本/contab.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 BACKUPDIR=/root/
 LOGFILE=$BACKUPDIR/mysql_logicbak.log
 TIME=`date +"%Y-%m-%d %H:%M:%S"`
@@ -7,6 +7,8 @@ echo "########################################" >> $LOGFILE
 echo "             backuping...               " >> $LOGFILE
 echo "########################################" >> $LOGFILE
 echo "$TIME Logical backup start..." >> $LOGFILE
-python mysqlbackup.py >>$LOGFILE
+source /etc/profile
+source ~/.bash_profile
+/usr/bin/python /opt/3.py >>$LOGFILE
 
 echo "$TIME script is end...." >> $LOGFILE
